a488728a6431659c9c6769a660aa182ea07244f1
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2021-11-30 16:14:57 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c1qkrvkwkomt.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "cecelialimjieshi-employee"
+customregion = "us-east-1"
