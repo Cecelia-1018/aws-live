a59e89b2dadcbfce2fa4156a7d09805cbd2eee88
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2021-12-05 13:22:41 +0800

    try

--- EmpApp.py
+++ EmpApp.py
@@ -48,7 +48,7 @@ def fetchdata():
             emp_id= cursor.fetchall()  
             print(emp_id)
             
-            return render_template('GetEmpOutput.html', id=emp_id)
+            return render_template('GetEmpOutput.html', emp=emp_id)
         except Exception as e:
             return str(e)
     else:
