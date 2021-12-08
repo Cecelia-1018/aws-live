1d22e09f66faee8a251bff6f188860c3585a093c
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2021-12-08 14:43:18 +0800

    check image url and the update statement

--- EmpApp.py
+++ EmpApp.py
@@ -155,7 +155,7 @@ def EditEmp():
         emp_id = request.form['emp_id']
         emp_image_file = request.files['emp_image_file']
 
-        update_sql = "UPDATE employee SET first_name = %s, last_name = %s, pri_skill = %s, location = %s WHERE emp_id = %s"
+        update_sql = "UPDATE employee SET emp_id = %s, first_name = %s, last_name = %s, pri_skill = %s, location = %s WHERE emp_id = %s, first_name = %s, last_name = %s, pri_skill = %s, location = %s"
         cursor = db_conn.cursor()
 
         try:
