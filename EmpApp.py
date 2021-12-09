f8e984ccf4682b00b512e5dae8184ed5515bb3bc
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2021-12-09 17:24:22 +0800

    try

--- EmpApp.py
+++ EmpApp.py
@@ -98,9 +98,10 @@ def DeleteEmp():
 
 @app.route('/view-attendance', methods=['GET','POST'])
 def ViewAttendance():
-    att_emp_sql = "SELECT employee.first_name, employee.last_name, attendance.date, attendance.time, attendance.att_values FROM attendance INNER JOIN employee ON attendance.emp_id = employee.emp_id"
+    date = request.form['date']
+    att_emp_sql = "SELECT employee.first_name, employee.last_name, attendance.date, attendance.time, attendance.att_values FROM attendance INNER JOIN employee ON attendance.emp_id = employee.emp_id WHERE date = %s"
     cursor = db_conn.cursor()
-    cursor.execute(att_emp_sql)
+    cursor.execute(att_emp_sql,(date))
     att_result= cursor.fetchall()  
     return render_template('ViewAttendance.html',  att_result=att_result)
         
