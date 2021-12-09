466de6aea958854ef78a1d9c1c88f62afeaae6f6
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2021-12-09 16:24:42 +0800

    format result

--- EmpApp.py
+++ EmpApp.py
@@ -102,7 +102,11 @@ def ViewAttendance():
     cursor = db_conn.cursor()
     cursor.execute(att_emp_sql)
     att_result= cursor.fetchall()  
-    return render_template('ViewAttendance.html', att_result=att_result)
+    
+    format_result = '{0[0]:20s} {0[1]:10s} {0[2]:10s} {0[3]:10s} {0[4]:20s}'.format(att_result)
+    
+
+    return render_template('ViewAttendance.html', att_result=format_result)
         
 
 @app.route('/attendance-emp', methods=['GET','POST'])
